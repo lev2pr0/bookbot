4eff36942fa3708e6f7edccf7e08b243afdec937
Levar Norwood <levar.norwood@outlook.com>
2025-05-25 00:10:44 -0400

    print report lesson + comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from stats import get_word_count
+# Import from stats.py
+from stats import get_word_count,get_char_count,sort_char
 
+# Function to return book content
 def get_book_text(filepath):
     file_contents = str()
     with open(filepath) as f:
@@ -8,8 +10,26 @@ def get_book_text(filepath):
 
 def main():
     filepath = "./books/frankenstein.txt"
+
+    # Get book content
     file_contents = get_book_text(filepath)
+
+    # Get word & character count with sorting results
     num_words = get_word_count(file_contents)
-    print(f"{num_words} words found in the document")
+    num_char = get_char_count(file_contents)
+    results = sort_char(num_char)
+
+    # Print report with excluding non-alphabetic characters from count
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for count in results:
+        if str.isalpha(count["char"]) == True:
+            print(f"{count["char"]}: {count["num"]}")
+        else:
+            pass
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,31 @@
 def get_word_count(text):
     word_list = str.split(text)
     return len(word_list)
+
+def get_char_count(text):
+    converted_char = str.lower(text)
+    char_count = {}
+    for char in converted_char:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+def sort_char(char_count):
+
+    def get_dict_list(char_count):
+        dict_list = []
+        dict_store = {}
+        for char in char_count:
+            num = char_count[char]
+            dict_store = dict(char=char, num=num)
+            dict_list.append(dict_store)
+        return dict_list
+
+    def sort_on(dict_list):
+        return dict_list["num"]
+
+    get_dict_list(char_count)
+    sorted_list = sorted(get_dict_list(char_count), reverse=True, key=sort_on)
+    return sorted_list
